d6e92905f7c50decf435b27b45c687b9487534ed
Keith Noguchi <keith@noguchi.us>
2023-03-07 22:44:49 -0800

    ch01: std::sync::Condvar example (#1)
    * MSRV 1.63 for the scoped thread

--- ch01/src/main.rs
+++ ch01/src/main.rs
@@ -1,3 +1,34 @@
+use std::collections::VecDeque;
+use std::sync::{Condvar, Mutex};
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let q = Mutex::new(VecDeque::new());
+    let ready = Condvar::new();
+
+    thread::scope(|s| {
+        // consumers.
+        for _ in 0..100 {
+            s.spawn(|| {
+                let mut q = q.lock().unwrap();
+                let item = loop {
+                    if let Some(item) = q.pop_front() {
+                        break item;
+                    } else {
+                        q = ready.wait(q).unwrap();
+                    }
+                };
+                drop(q);
+                dbg!(item);
+            });
+        }
+
+        // a producer.
+        for i in 0.. {
+            q.lock().unwrap().push_back(i);
+            ready.notify_one();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
 }
