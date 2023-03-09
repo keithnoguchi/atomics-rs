d6b14e4d371d958880e474484a849ce97154ea72
Keith Noguchi <keith@noguchi.us>
2023-03-09 09:42:30 -0800

    ch02: A counter with compare_exchange()

--- ch02/src/main.rs
+++ ch02/src/main.rs
@@ -1,62 +1,45 @@
-//! Statistics with `fetch_add()` and `fetch_max()`
+//! Counter with compare_exchange, instead of fetch_add
 
-use std::sync::atomic::AtomicU64;
-use std::sync::atomic::AtomicUsize;
+use std::sync::atomic::AtomicU32;
 use std::sync::atomic::Ordering::Relaxed;
 use std::thread;
-use std::time::{Duration, Instant};
+use std::time::Duration;
 
 fn main() {
-    const WORKERS: usize = 4;
-    const JOBS: usize = 100;
-    let processed = AtomicUsize::new(0);
-    let total_time = AtomicU64::new(0);
-    let max_time = AtomicU64::new(0);
+    let counter = &AtomicU32::new(0);
+    let increment = || {
+        let mut current = counter.load(Relaxed);
+        loop {
+            let new = current + 1;
+            match counter.compare_exchange(current, new, Relaxed, Relaxed) {
+                Ok(_) => return,
+                Err(v) => current = v,
+            }
+        }
+    };
+    let work = |id| {
+        let delay = 100 * id as u64;
+        thread::sleep(Duration::from_millis(delay));
+        increment();
+    };
 
     thread::scope(|s| {
         // workers.
-        let work = &|_id, processed: &AtomicUsize| {
-            thread::sleep(Duration::from_millis(98));
-            processed.fetch_add(1, Relaxed);
-        };
-        let next_id = &|| {
-            static NEXT_ID: AtomicUsize = AtomicUsize::new(0);
-            NEXT_ID.fetch_add(1, Relaxed)
-        };
-        s.spawn(|| {
-            (0..WORKERS).for_each(|_id| {
-                let id = next_id();
-                (0..(JOBS / WORKERS)).for_each(|item_id| {
-                    let start = Instant::now();
-                    work((JOBS / WORKERS) * id + item_id, &processed);
-                    let duration = start.elapsed().as_micros() as u64;
-                    total_time.fetch_add(duration, Relaxed);
-                    max_time.fetch_max(duration, Relaxed);
-                });
+        (0..10).for_each(|id| {
+            s.spawn(move || {
+                (0..20).for_each(|_| work(id));
+                println!("worker{id} done");
             });
         });
 
-        // A reporter.
-        let report = |processed| {
-            let duration = Duration::from_micros(total_time.load(Relaxed));
-            let peak = Duration::from_micros(max_time.load(Relaxed));
-            println!(
-                "{}/{} processed, {:?} process time, {:?} peak duration.",
-                processed, JOBS, duration, peak,
-            );
-        };
+        // a reporter.
         loop {
-            let processed = processed.load(Relaxed);
-            if processed == 0 {
-                thread::sleep(Duration::from_secs(1));
-                continue;
-            } else if processed == JOBS {
-                report(processed);
+            let count = counter.load(Relaxed);
+            println!("current count is {count}");
+            if count == 200 {
                 break;
-            } else {
-                report(processed);
-                thread::sleep(Duration::from_secs(1));
             }
+            thread::sleep(Duration::from_secs(1));
         }
     });
 }
