c312483db1889780f7bd2c333f524b53b32f1713
Keith Noguchi <keith@noguchi.us>
2023-03-08 22:41:09 -0800

    ch02: global ID example

--- ch02/src/main.rs
+++ ch02/src/main.rs
@@ -15,14 +15,21 @@ fn main() {
 
     thread::scope(|s| {
         // workers.
-        let work = &|_id| thread::sleep(Duration::from_millis(98));
+        let work = &|_id, processed: &AtomicUsize| {
+            thread::sleep(Duration::from_millis(98));
+            processed.fetch_add(1, Relaxed);
+        };
+        let next_id = &|| {
+            static NEXT_ID: AtomicUsize = AtomicUsize::new(0);
+            NEXT_ID.fetch_add(1, Relaxed)
+        };
         s.spawn(|| {
-            (0..WORKERS).for_each(|id| {
+            (0..WORKERS).for_each(|_id| {
+                let id = next_id();
                 (0..(JOBS / WORKERS)).for_each(|item_id| {
                     let start = Instant::now();
-                    work((JOBS / WORKERS) * id + item_id);
+                    work((JOBS / WORKERS) * id + item_id, &processed);
                     let duration = start.elapsed().as_micros() as u64;
-                    processed.fetch_add(1, Relaxed);
                     total_time.fetch_add(duration, Relaxed);
                     max_time.fetch_max(duration, Relaxed);
                 });
