a20a4acbed2e52af4e28fde79910ff9457f77e25
Keith Noguchi <keith@noguchi.us>
2023-03-09 16:29:26 -0800

    ch03: unsafe version of Acquire and Release synchronization

--- ch03/src/main.rs
+++ ch03/src/main.rs
@@ -1,28 +1,26 @@
-//! Acquire and Release memory ordering
+//! Acquire and Release memory ordering (unsafe)
 
 use std::sync::atomic::AtomicBool;
-use std::sync::atomic::AtomicI32;
-use std::sync::atomic::Ordering::{Acquire, Relaxed, Release};
+use std::sync::atomic::Ordering::{Acquire, Release};
 use std::thread;
 use std::time::Duration;
 
 fn main() {
-    static DATA: AtomicI32 = AtomicI32::new(0);
     static READY: AtomicBool = AtomicBool::new(false);
+    static mut DATA: i32 = 0;
 
     let main_thread = thread::current();
     thread::spawn(move || {
-        DATA.store(123, Relaxed);
-        thread::sleep(Duration::from_millis(500));
-        READY.store(true, Release); // everything before `Release`
-                                    // happens before across threads.
+        unsafe {
+            DATA = 123;
+        }
+        READY.store(true, Release);
         main_thread.unpark();
     });
 
     while !READY.load(Acquire) {
-        // everything after `Acquire` happens after across threads.
-        println!("waiting...");
+        println!("waiting worker...");
         thread::park_timeout(Duration::from_secs(1));
     }
-    assert_eq!(DATA.load(Relaxed), 123);
+    assert_eq!(unsafe { DATA }, 123);
 }
