f0c5a1a31ed6a18f93bfdc2058a82f85ebfe2cfb
Keith Noguchi <keith@noguchi.us>
2023-03-09 22:13:16 -0800

    A memory fence with Acquire, Relaxed, and Release

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Kudos to [Mara Bos].
 ## Examples
 
 - [Chapter 1: A channel with `Mutex` and `Condvar`](ch01/src/main.rs)
-- [Chapter 2: Lazy one-time atomic value initialization](ch02/src/main.rs)
-- [Chapter 3: Lazy one-time non-atomic value initialization](ch03/src/main.rs)
+- [Chapter 2: A lazy one-time atomic value initialization](ch02/src/main.rs)
+- [Chapter 3: A memory fence with `Acquire`, `Relaxed`, and `Release`](ch03/src/main.rs)
 
 Happy Hacking!
--- ch03/src/main.rs
+++ ch03/src/main.rs
@@ -1,60 +1,46 @@
-//! A lazy one-time non-atomic value initialization
+//! A memory fence with Acquire, Relaxed, and Release
 
-use std::collections::HashMap;
-use std::ptr;
-use std::sync::atomic::AtomicPtr;
-use std::sync::atomic::Ordering::{Acquire, Release};
-use std::sync::{Arc, Mutex};
+use std::array;
+use std::sync::atomic::fence;
+use std::sync::atomic::AtomicBool;
+use std::sync::atomic::Ordering::{Acquire, Relaxed, Release};
 use std::thread;
 use std::time::Duration;
 
-#[derive(PartialEq)]
-struct Data;
+static mut DATA: [u32; 10] = [0; 10];
+
+#[allow(clippy::declare_interior_mutable_const)]
+const ATOMIC_FALSE: AtomicBool = AtomicBool::new(false);
+static READY: [AtomicBool; 10] = [ATOMIC_FALSE; 10];
 
 fn main() {
-    let get_data = || -> &'static Data {
-        static PTR: AtomicPtr<Data> = AtomicPtr::new(ptr::null_mut());
-
-        let mut p = PTR.load(Acquire);
-        if p.is_null() {
-            let data = Box::into_raw(Box::new(Data));
-            if let Err(v) = PTR.compare_exchange(ptr::null_mut(), data, Release, Acquire) {
-                // lose the race.
-                drop(unsafe { Box::from_raw(p) });
-                p = v;
-            }
-        }
-        unsafe { &*p }
+    let work = |id: usize| {
+        let id = id as u32;
+        thread::sleep(Duration::from_millis(id as u64 % 3 + 1));
+        id
     };
-
-    let result = Arc::new(Mutex::new(HashMap::new()));
-    let main_thread = &thread::current();
-    thread::scope(|s| {
-        // get the data from 100 workers.
-        (0..100).for_each(|id| {
-            let result = result.clone();
-            s.spawn(move || {
-                result.lock().unwrap().insert(id, get_data());
-                main_thread.unpark();
-            });
-        });
-
-        // wait for the completion.
-        loop {
-            let result = result.lock().unwrap().len();
-            if result == 100 {
-                break;
+    // workers.
+    for i in 0..10 {
+        thread::spawn(move || {
+            let data = work(i);
+            unsafe {
+                DATA[i] = data;
             }
-            println!("waiting...");
-            thread::park_timeout(Duration::from_secs(1));
+            READY[i].store(true, Release);
+        });
+    }
+
+    thread::sleep(Duration::from_millis(2));
+
+    // a repoter.
+    let ready: [bool; 10] = array::from_fn(|i| READY[i].load(Relaxed));
+    if ready.contains(&true) {
+        fence(Acquire);
+        for i in 0..10 {
+            if ready[i] {
+                let data = unsafe { DATA[i] };
+                println!("DATA[{i}] = {data}");
+            }
         }
-        let expected = get_data();
-        let correct = result
-            .lock()
-            .unwrap()
-            .values()
-            .filter(|&got| *got == expected)
-            .count();
-        assert_eq!(correct, 100);
-    });
+    }
 }
