ff49699ef5c160c830bee1d5aac5adb79d361635
Keith Noguchi <keith@noguchi.us>
2023-03-10 10:29:04 -0800

    ch04: Initial version of the spin lock
    It's super slow, but works(TM). :)

--- ch04/src/main.rs
+++ ch04/src/main.rs
@@ -1,3 +1,62 @@
+//! A super slow, but working, spin lock.
+
+use std::sync::atomic::AtomicBool;
+use std::sync::atomic::Ordering::{Acquire, Relaxed, Release};
+use std::thread;
+use std::time::Duration;
+
+pub struct SpinLock {
+    locked: AtomicBool,
+}
+
+impl SpinLock {
+    pub const fn new() -> Self {
+        Self {
+            locked: AtomicBool::new(false),
+        }
+    }
+
+    pub fn lock(&self) {
+        while self
+            .locked
+            .compare_exchange(false, true, Acquire, Relaxed)
+            .is_err()
+        {
+            std::hint::spin_loop();
+        }
+    }
+
+    pub fn unlock(&self) {
+        self.locked.store(false, Release);
+    }
+}
+
+static LOCK: SpinLock = SpinLock::new();
+
 fn main() {
-    println!("Hello, world!");
+    static mut DATA: u8 = 0;
+    let increment = || {
+        LOCK.lock();
+        unsafe {
+            DATA += 1;
+        }
+        LOCK.unlock();
+    };
+
+    // workers.
+    for _i in 0..100 {
+        thread::spawn(increment);
+    }
+
+    // a validator.
+    let get = || -> u8 {
+        LOCK.lock();
+        let result = unsafe { DATA };
+        LOCK.unlock();
+        result
+    };
+    while get() != 100 {
+        thread::park_timeout(Duration::from_secs(1));
+    }
+    assert_eq!(get(), 100);
 }
