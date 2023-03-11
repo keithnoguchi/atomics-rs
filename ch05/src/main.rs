0892789e077a243ae3e5d42942843df81a2bded8
Keith Noguchi <keith@noguchi.us>
2023-03-10 21:59:09 -0800

    ch05: A safe channel through panic
    Uses AtomicU8, instead of double AtomicBool.

--- ch05/src/main.rs
+++ ch05/src/main.rs
@@ -3,15 +3,19 @@
 
 use std::cell::UnsafeCell;
 use std::mem::MaybeUninit;
-use std::sync::atomic::AtomicBool;
+use std::sync::atomic::AtomicU8;
 use std::sync::atomic::Ordering::{Acquire, Relaxed, Release};
 use std::thread;
+
+const EMPTY: u8 = 0;
+const WRITING: u8 = 1;
+const READY: u8 = 2;
+const READING: u8 = 3;
 
 #[derive(Debug)]
 pub struct Channel<T> {
     msg: UnsafeCell<MaybeUninit<T>>,
-    in_use: AtomicBool,
-    ready: AtomicBool,
+    state: AtomicU8,
 }
 
 unsafe impl<T> Sync for Channel<T> where T: Send {}
@@ -20,8 +24,7 @@ impl<T> Default for Channel<T> {
     fn default() -> Self {
         Self {
             msg: UnsafeCell::new(MaybeUninit::uninit()),
-            in_use: AtomicBool::new(false),
-            ready: AtomicBool::new(false),
+            state: AtomicU8::new(EMPTY),
         }
     }
 }
@@ -32,27 +35,35 @@ impl<T> Channel<T> {
     }
 
     pub fn is_ready(&self) -> bool {
-        self.ready.load(Relaxed)
+        self.state.load(Relaxed) == READY
     }
 
     /// # Panics
     ///
     /// It panics with multiple sends.
     pub fn send(&self, msg: T) {
-        if self.in_use.swap(true, Relaxed) {
+        if self
+            .state
+            .compare_exchange(EMPTY, WRITING, Relaxed, Relaxed)
+            .is_err()
+        {
             panic!("it's already in use");
         }
         unsafe {
             (*self.msg.get()).write(msg);
         }
-        self.ready.store(true, Release);
+        self.state.store(READY, Release);
     }
 
     /// # Panics
     ///
     /// It panics when there is no message.
     pub fn recv(&self) -> T {
-        if !self.ready.load(Acquire) {
+        if self
+            .state
+            .compare_exchange(READY, READING, Acquire, Relaxed)
+            .is_err()
+        {
             panic!("there is no message");
         }
         unsafe { (*self.msg.get()).assume_init_read() }
