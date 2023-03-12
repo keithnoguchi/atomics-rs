29d9733cc91f9b3803aa2c7e659402dd327f7848
Keith Noguchi <keith@noguchi.us>
2023-03-11 16:12:31 -0800

    ch05 A safe one-shot channel with borrow, instead Arc

--- README.md
+++ README.md
@@ -13,6 +13,6 @@ Kudos to [Mara Bos].
 - [Chapter 2: A lazy one-time atomic value initialization](ch02/src/main.rs)
 - [Chapter 3: A memory fence with `Acquire`, `Relaxed`, and `Release`](ch03/src/main.rs)
 - [Chapter 4: A *safe* spin lock](ch04/src/main.rs)
-- [Chapter 5: A *safe* one-shot channel through types](ch05/src/main.rs)
+- [Chapter 5: A *safe* one-shot borrowed channel](ch05/src/main.rs)
 
 Happy Hacking!
--- ch05/src/main.rs
+++ ch05/src/main.rs
@@ -1,32 +1,62 @@
-//! A safe one-shot channel through types.
+//! A safe one-shot borrowed channel.
+
 #![forbid(missing_debug_implementations)]
 
 use std::cell::UnsafeCell;
-use std::collections::HashMap;
 use std::mem::MaybeUninit;
 use std::sync::atomic::AtomicBool;
 use std::sync::atomic::Ordering::{Acquire, Relaxed, Release};
-use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
 
-pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
-    let channel = Arc::new(Channel::default());
+#[derive(Debug)]
+pub struct Channel<T> {
+    ready: AtomicBool,
+    data: UnsafeCell<MaybeUninit<T>>,
+}
 
-    (
-        Sender {
-            channel: channel.clone(),
-        },
-        Receiver { channel },
-    )
+unsafe impl<T> Sync for Channel<T> where T: Send {}
+
+impl<T> Drop for Channel<T> {
+    fn drop(&mut self) {
+        if self.ready.load(Acquire) {
+            unsafe { (*self.data.get_mut()).assume_init_drop() }
+        }
+    }
+}
+
+impl<T> Channel<T> {
+    pub const fn new() -> Self {
+        Self {
+            ready: AtomicBool::new(false),
+            data: UnsafeCell::new(MaybeUninit::uninit()),
+        }
+    }
+
+    /// Splits the `Channel` into `Sender` and `Receiver`.
+    ///
+    /// It takes the exclusive reference to avoid the reuse,
+    /// as it's a single shot channel.
+    pub fn split(&mut self) -> (Sender<T>, Receiver<T>) {
+        // drops the previous channel
+        *self = Self::new();
+        (
+            Sender {
+                channel: self, // shared ref.
+            },
+            Receiver {
+                channel: self, // shared ref.
+            },
+        )
+    }
 }
 
 #[derive(Debug)]
-pub struct Sender<T> {
-    channel: Arc<Channel<T>>,
+pub struct Sender<'a, T> {
+    channel: &'a Channel<T>,
 }
 
-impl<T> Sender<T> {
+impl<T> Sender<'_, T> {
     pub fn send(self, msg: T) {
         unsafe {
             (*self.channel.data.get()).write(msg);
@@ -36,72 +66,68 @@ impl<T> Sender<T> {
 }
 
 #[derive(Debug)]
-pub struct Receiver<T> {
-    channel: Arc<Channel<T>>,
+pub struct Receiver<'a, T> {
+    channel: &'a Channel<T>,
 }
 
-impl<T> Receiver<T> {
+impl<T> Receiver<'_, T> {
     pub fn is_ready(&self) -> bool {
         self.channel.ready.load(Relaxed)
     }
 
     /// # Panics
     ///
-    /// It panics when there is no message on the channel yet.
+    /// It panics when the data is not ready.
+    /// check the data readiness by calling
+    /// `is_ready()`, instead.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use std::thread;
+    ///
+    /// let mut channel = Channel::new()
+    /// let (tx, rx) = channel.split();
+    ///
+    /// thread::scope(|s| {
+    ///     s.spawn(|| {
+    ///         tx.send("Test".to_string());
+    ///     });
+    ///
+    ///     s.spawn(|| {
+    ///         while !rx.is_ready() {
+    ///             thread::park();
+    ///         }
+    ///         let msg = rx.receive();
+    ///         assert_eq!(msg, "Test".to_string());
+    ///     });
+    /// });
+    /// ```
     pub fn receive(self) -> T {
         if !self.channel.ready.swap(false, Acquire) {
-            panic!("no message on the channel");
+            panic!("message is not ready");
         }
         unsafe { (*self.channel.data.get()).assume_init_read() }
     }
 }
 
-#[derive(Debug)]
-struct Channel<T> {
-    ready: AtomicBool,
-    data: UnsafeCell<MaybeUninit<T>>,
-}
-
-unsafe impl<T> Sync for Channel<T> where T: Send {}
-
-impl<T> Default for Channel<T> {
-    fn default() -> Self {
-        Self {
-            ready: AtomicBool::new(false),
-            data: UnsafeCell::new(MaybeUninit::uninit()),
-        }
-    }
-}
-
-impl<T> Drop for Channel<T> {
-    fn drop(&mut self) {
-        if self.ready.load(Acquire) {
-            unsafe {
-                (*self.data.get()).assume_init_drop();
-            }
-        }
-    }
-}
-
 fn main() {
-    let (tx, rx) = channel();
-    let consumer = thread::current();
+    let mut channel = Channel::new();
+    let (tx, rx) = channel.split();
 
+    let t = thread::current();
     thread::scope(|s| {
-        // a producer.
+        // a sender.
         s.spawn(|| {
-            let mut state = HashMap::new();
-            state.insert("hello".to_string(), "world");
-            tx.send(state);
-            consumer.unpark();
+            tx.send("Let's do this!".to_string());
+            t.unpark();
         });
-    });
 
-    // a consumer.
-    if !rx.is_ready() {
-        thread::park_timeout(Duration::from_secs(1));
-    }
-    let state = rx.receive();
-    assert!(state.len() == 1);
-    assert_eq!(state.get("hello"), Some(&"world"));
+        // a receiver.
+        while !rx.is_ready() {
+            thread::park_timeout(Duration::from_secs(1));
+        }
+        let msg = rx.receive();
+        assert_eq!(msg, String::from("Let's do this!"));
+    });
 }
