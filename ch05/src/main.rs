cf5e9e0b37b4d2f11f066d85d57e09ee078ea036
Keith Noguchi <keith@noguchi.us>
2023-03-11 17:05:00 -0800

    ch05: An one-shot channel

--- ch05/src/main.rs
+++ ch05/src/main.rs
@@ -1,13 +1,12 @@
-//! A safe one-shot borrowed channel.
+//! An one-shot channel
 
 #![forbid(missing_debug_implementations)]
 
 use std::cell::UnsafeCell;
 use std::mem::MaybeUninit;
 use std::sync::atomic::AtomicBool;
-use std::sync::atomic::Ordering::{Acquire, Relaxed, Release};
-use std::thread;
-use std::time::Duration;
+use std::sync::atomic::Ordering::{Acquire, Release};
+use std::thread::{self, Thread};
 
 #[derive(Debug)]
 pub struct Channel<T> {
@@ -19,6 +18,7 @@ unsafe impl<T> Sync for Channel<T> where T: Send {}
 
 impl<T> Drop for Channel<T> {
     fn drop(&mut self) {
+        println!("dropping...");
         if self.ready.load(Acquire) {
             unsafe { (*self.data.get_mut()).assume_init_drop() }
         }
@@ -33,20 +33,16 @@ impl<T> Channel<T> {
         }
     }
 
-    /// Splits the `Channel` into `Sender` and `Receiver`.
-    ///
-    /// It takes the exclusive reference to avoid the reuse,
-    /// as it's a single shot channel.
     pub fn split(&mut self) -> (Sender<T>, Receiver<T>) {
-        // drops the previous channel
+        // force drop the previous channel, if it's still
+        // not split.
         *self = Self::new();
         (
             Sender {
-                channel: self, // shared ref.
+                channel: self,
+                receiver: thread::current(),
             },
-            Receiver {
-                channel: self, // shared ref.
-            },
+            Receiver { channel: self },
         )
     }
 }
@@ -54,6 +50,7 @@ impl<T> Channel<T> {
 #[derive(Debug)]
 pub struct Sender<'a, T> {
     channel: &'a Channel<T>,
+    receiver: Thread,
 }
 
 impl<T> Sender<'_, T> {
@@ -62,6 +59,7 @@ impl<T> Sender<'_, T> {
             (*self.channel.data.get()).write(msg);
         }
         self.channel.ready.store(true, Release);
+        self.receiver.unpark();
     }
 }
 
@@ -71,41 +69,9 @@ pub struct Receiver<'a, T> {
 }
 
 impl<T> Receiver<'_, T> {
-    pub fn is_ready(&self) -> bool {
-        self.channel.ready.load(Relaxed)
-    }
-
-    /// # Panics
-    ///
-    /// It panics when the data is not ready.
-    /// check the data readiness by calling
-    /// `is_ready()`, instead.
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// use std::thread;
-    ///
-    /// let mut channel = Channel::new()
-    /// let (tx, rx) = channel.split();
-    ///
-    /// thread::scope(|s| {
-    ///     s.spawn(|| {
-    ///         tx.send("Test".to_string());
-    ///     });
-    ///
-    ///     s.spawn(|| {
-    ///         while !rx.is_ready() {
-    ///             thread::park();
-    ///         }
-    ///         let msg = rx.receive();
-    ///         assert_eq!(msg, "Test".to_string());
-    ///     });
-    /// });
-    /// ```
     pub fn receive(self) -> T {
-        if !self.channel.ready.swap(false, Acquire) {
-            panic!("message is not ready");
+        while !self.channel.ready.swap(false, Acquire) {
+            thread::park();
         }
         unsafe { (*self.channel.data.get()).assume_init_read() }
     }
@@ -115,19 +81,14 @@ fn main() {
     let mut channel = Channel::new();
     let (tx, rx) = channel.split();
 
-    let t = thread::current();
+    // a producer.
     thread::scope(|s| {
-        // a sender.
         s.spawn(|| {
-            tx.send("Let's do this!".to_string());
-            t.unpark();
+            tx.send("Hello, World!".to_string());
         });
 
-        // a receiver.
-        while !rx.is_ready() {
-            thread::park_timeout(Duration::from_secs(1));
-        }
-        let msg = rx.receive();
-        assert_eq!(msg, String::from("Let's do this!"));
+        // a consumer.
+        let message = rx.receive();
+        assert_eq!(message, String::from("Hello, World!"));
     });
 }
