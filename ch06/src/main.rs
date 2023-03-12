cf00e81a58f0d08b7bca2f458ed6e1032d25ef25
Keith Noguchi <keith@noguchi.us>
2023-03-12 14:06:56 -0700

    ch06: start over Arc<T> and Weak<T>

--- ch06/src/main.rs
+++ ch06/src/main.rs
@@ -3,27 +3,53 @@
 #![forbid(missing_debug_implementations)]
 
 use std::cell::UnsafeCell;
+use std::ptr::NonNull;
 use std::sync::atomic::AtomicUsize;
+use std::sync::atomic::Ordering::{Acquire, Relaxed};
 
 #[derive(Debug)]
-struct Data<T> {
-    _data_ref_count: AtomicUsize,
-    _alloc_ref_count: AtomicUsize,
-    _data: UnsafeCell<Option<T>>,
+pub struct Arc<T> {
+    weak: Weak<T>,
 }
 
-impl<T> Default for Data<T> {
-    fn default() -> Self {
+impl<T> Arc<T> {
+    pub fn new(data: T) -> Self {
         Self {
-            _data_ref_count: AtomicUsize::new(0),
-            _alloc_ref_count: AtomicUsize::new(0),
-            _data: UnsafeCell::new(None),
+            weak: Weak {
+                ptr: NonNull::from(Box::leak(Box::new(Data {
+                    data_ref_count: AtomicUsize::new(1),
+                    alloc_ref_count: AtomicUsize::new(1),
+                    data: UnsafeCell::new(Some(data)),
+                }))),
+            },
         }
     }
 }
 
-fn main() {
-    let data = Data::<String>::default();
+#[derive(Debug)]
+pub struct Weak<T> {
+    ptr: NonNull<Data<T>>,
+}
 
+#[derive(Debug)]
+struct Data<T> {
+    data_ref_count: AtomicUsize,
+    alloc_ref_count: AtomicUsize,
+    data: UnsafeCell<Option<T>>,
+}
+
+fn main() {
+    static DROP_COUNT: AtomicUsize = AtomicUsize::new(0);
+    #[derive(Debug)]
+    struct DropMonitor;
+    impl Drop for DropMonitor {
+        fn drop(&mut self) {
+            DROP_COUNT.fetch_add(1, Relaxed);
+        }
+    }
+
+    let data = Arc::new((String::from("hello"), DropMonitor));
     println!("{data:?}");
+    drop(data);
+    assert_eq!(DROP_COUNT.load(Acquire), 1);
 }
