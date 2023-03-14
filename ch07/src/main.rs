8c12db81d45ee0d785045dd24d8d56cf82d41cd1
Keith Noguchi <keith@noguchi.us>
2023-03-14 08:07:38 -0700

    ch07: Conditional compilation
    Checks the RUSTUP_TOOLCHAIN to decide if
    we use the nightly feature.

--- ch07/src/main.rs
+++ ch07/src/main.rs
@@ -1,3 +1,24 @@
+//! Understanding Processors
+
+use std::sync::atomic::AtomicU64;
+use std::sync::atomic::Ordering::Relaxed;
+use std::time::Instant;
+
+#[cfg(feature = "nightly-features")]
+use std::hint::black_box;
+
+#[cfg(not(feature = "nightly-features"))]
+pub const fn black_box<T>(dummy: T) -> T {
+    dummy
+}
+
+static A: AtomicU64 = AtomicU64::new(0);
+
 fn main() {
-    println!("Hello, world!");
+    let start = Instant::now();
+    black_box(&A);
+    for _ in 0..1_000_000_000 {
+        black_box(A.load(Relaxed));
+    }
+    println!("{:?}", start.elapsed());
 }
