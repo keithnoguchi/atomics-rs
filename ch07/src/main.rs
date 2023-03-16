086a77a420e609a7c79a7ee11a1a74ce1df0161a
Keith Noguchi <keith@noguchi.us>
2023-03-15 22:55:33 -0700

    ch07: Cache line alignment
    Make 64 bytes apart to avoid
    the influence of the other
    threads.

--- ch07/src/main.rs
+++ ch07/src/main.rs
@@ -1,4 +1,10 @@
 //! Understanding Processors
+//!
+//! # Examples
+//!
+//! ```
+//! $ cargo +nightly run --release
+//! ```
 
 use std::sync::atomic::AtomicU64;
 use std::sync::atomic::Ordering::Relaxed;
@@ -17,23 +23,26 @@ const fn black_box<T>(dummy: T) -> T {
     dummy
 }
 
-static A: [AtomicU64; 3] = [
-    AtomicU64::new(0),
-    AtomicU64::new(0),
-    AtomicU64::new(0),
+/// 64 byte alignment.
+#[repr(align(64))]
+struct Aligned(AtomicU64);
+
+static A: [Aligned; 3] = [
+    Aligned(AtomicU64::new(0)),
+    Aligned(AtomicU64::new(0)),
+    Aligned(AtomicU64::new(0)),
 ];
 
 fn main() {
     black_box(&A);
 
     thread::spawn(|| loop {
-        A[0].store(0, Relaxed);
-        A[2].store(0, Relaxed);
+        A[0].0.store(0, Relaxed);
     });
 
     let start = Instant::now();
     for _ in 0..1_000_000_000 {
-        black_box(A[1].load(Relaxed));
+        black_box(A[1].0.load(Relaxed));
     }
     println!("{:?}", start.elapsed());
 }
