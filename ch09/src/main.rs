276cadd8159c74d45c8db7979e338327980de520
Keith Noguchi <keith@noguchi.us>
2023-03-28 08:25:45 -0700

    ch09: Mutex<T> type

--- ch09/src/main.rs
+++ ch09/src/main.rs
@@ -1,3 +1,27 @@
+//! A mutex
+
+#![forbid(missing_debug_implementations)]
+
+use std::cell::UnsafeCell;
+use std::sync::atomic::AtomicU32;
+
+#[derive(Debug)]
+pub struct Mutex<T> {
+    state: AtomicU32,
+    value: UnsafeCell<T>,
+}
+
+impl<T> Mutex<T> {
+    pub const fn new(value: T) -> Self {
+        Self {
+            state: AtomicU32::new(0),
+            value: UnsafeCell::new(value),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let counter = Mutex::new(0);
+
+    dbg!(counter);
 }
