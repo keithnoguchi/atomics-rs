85a2618570bd5340b9db7d2d2b75bb8a5b5d98eb
Keith Noguchi <keith@noguchi.us>
2023-03-31 10:12:10 -0700

    at least 4 times faster than single state version!

--- ch09/src/main.rs
+++ ch09/src/main.rs
@@ -4,15 +4,19 @@
 //!
 //! ```
 //! $ cargo +nightly run -rq
+//! 20000000 locks in 1.496873575s
 //! ```
 
 #![forbid(missing_debug_implementations)]
 
 use std::cell::UnsafeCell;
+use std::ops::{Deref, DerefMut};
 use std::sync::atomic::AtomicU32;
 use std::sync::atomic::Ordering::{Acquire, Relaxed, Release};
 use std::thread;
 use std::time::Instant;
+
+use atomic_wait::{wait, wake_one};
 
 #[derive(Debug)]
 pub struct Mutex<T> {
@@ -20,12 +24,51 @@ pub struct Mutex<T> {
     value: UnsafeCell<T>,
 }
 
+unsafe impl<T> Send for Mutex<T> where T: Send {}
+unsafe impl<T> Sync for Mutex<T> where T: Send {}
+
 impl<T> Mutex<T> {
     pub const fn new(value: T) -> Self {
         Self {
             state: AtomicU32::new(0),
             value: UnsafeCell::new(value),
         }
+    }
+
+    pub fn lock(&self) -> Guard<'_, T> {
+        if self.state.compare_exchange(0, 1, Acquire, Relaxed).is_err() {
+            while self.state.swap(2, Acquire) != 0 {
+                wait(&self.state, 2);
+            }
+        }
+        Guard { lock: self }
+    }
+}
+
+#[derive(Debug)]
+pub struct Guard<'a, T> {
+    lock: &'a Mutex<T>,
+}
+
+impl<T> Drop for Guard<'_, T> {
+    fn drop(&mut self) {
+        if self.lock.state.swap(0, Release) == 2 {
+            wake_one(&self.lock.state);
+        }
+    }
+}
+
+impl<T> Deref for Guard<'_, T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        unsafe { &*self.lock.value.get() }
+    }
+}
+
+impl<T> DerefMut for Guard<'_, T> {
+    fn deref_mut(&mut self) -> &mut T {
+        unsafe { &mut *self.lock.value.get() }
     }
 }
 
@@ -39,11 +82,13 @@ fn main() {
         for _ in 0..4 {
             s.spawn(|| {
                 for _ in 0..5_000_000 {
+                    *m.lock() += 1;
                 }
             });
         }
     });
     let duration = start.elapsed();
 
-    println!("{:?}", duration);
+    println!("{} locks in {:?}", *m.lock(), duration);
+    assert_eq!(*m.lock(), 4 * 5_000_000);
 }
