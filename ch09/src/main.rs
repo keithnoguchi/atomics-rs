5adf2275e667152a132688015fa5c26c51cc6626
Keith Noguchi <keith@noguchi.us>
2023-04-02 09:04:50 -0700

    a vanilla spin lock

--- ch09/src/main.rs
+++ ch09/src/main.rs
@@ -1,15 +1,18 @@
-//! A Mutex<T> with spins
+//! A vanilla spinlock
 //!
 //! # Examples
 //!
 //! ```
 //! $ cargo +nightly run -rq
+//! 20000000 locks in 3.44102183s
 //! ```
 
 #![forbid(missing_debug_implementations)]
 
 use std::cell::UnsafeCell;
+use std::ops::{Deref, DerefMut};
 use std::sync::atomic::AtomicU32;
+use std::sync::atomic::Ordering::{Acquire, Release};
 use std::thread;
 use std::time::Instant;
 
@@ -29,6 +32,38 @@ impl<T> Mutex<T> {
             value: UnsafeCell::new(value),
         }
     }
+
+    pub fn lock(&self) -> Guard<'_, T> {
+        while self.state.swap(1, Acquire) != 0 {
+            std::hint::spin_loop();
+        }
+        Guard { lock: self }
+    }
+}
+
+#[derive(Debug)]
+pub struct Guard<'a, T> {
+    lock: &'a Mutex<T>,
+}
+
+impl<T> Drop for Guard<'_, T> {
+    fn drop(&mut self) {
+        self.lock.state.store(0, Release);
+    }
+}
+
+impl<T> Deref for Guard<'_, T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        unsafe { &*self.lock.value.get() }
+    }
+}
+
+impl<T> DerefMut for Guard<'_, T> {
+    fn deref_mut(&mut self) -> &mut T {
+        unsafe { &mut *self.lock.value.get() }
+    }
 }
 
 fn main() {
@@ -39,10 +74,15 @@ fn main() {
     let start = Instant::now();
     thread::scope(|s| {
         for _ in 0..4 {
-            s.spawn(|| {});
+            s.spawn(|| {
+                for _ in 0..5_000_000 {
+                    *m.lock() += 1;
+                }
+            });
         }
     });
     let duration = start.elapsed();
 
-    println!("{:?}", duration);
+    println!("{} locks in {:?}", 4 * 5_000_000, duration);
+    assert_eq!(*m.lock(), 4 * 5_000_000);
 }
